f80df4f3ce62eb83b806e443a862b6deb7c7802f
leminity <leminity@gmail.com>
2026-07-19 14:40:40 +0900

    build: restore transformer on Gradle 9

--- realm-transformer/src/main/kotlin/io/realm/transformer/RealmTransformer.kt
+++ realm-transformer/src/main/kotlin/io/realm/transformer/RealmTransformer.kt
@@ -252,7 +252,7 @@ abstract class RealmTransformerTask : DefaultTask() {
         val jarFileOutput: FileSystem = output.get().let { jarFile ->
             // Workaround to create the Jar if does not exist, as FileSystems fails to do so.
             touchJarFile(jarFile)
-            FileSystems.newFileSystem(output.get().asFile.toPath(), null)
+            FileSystems.newFileSystem(output.get().asFile.toPath(), null as ClassLoader?)
         }
 
         val build: BuildTemplate =
